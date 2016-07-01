907b09391927473a4ed6a7bb06054b55e5a3a89c
Zac Colley <hi@zaccolley.com>
2016-07-01 15:48:13 +0100

    Include scripts correctly and process with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
@@ -7,6 +10,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var kss = require('kss');
 var browserSync = require('browser-sync').create();
 var copy = require('gulp-copy');
+var rename = require('gulp-rename');
 
 var options = {
   autoprefixer: {
@@ -18,13 +22,36 @@ var options = {
     homepage: 'README.md',
     destination: './styleguide/',
     builder: './builder/',
-    css: 'css/main.css'
+    css: 'css/main.css',
+    js: 'js/main.min.js'
   }
 };
 
+var bootstrapPath = './node_modules/bootstrap-sass/assets/';
+
 var stylesheets = [
   options.styleguide.source + '**/*.scss',
-  './node_modules/bootstrap-sass/assets/stylesheets'
+  bootstrapPath + 'stylesheets'
+];
+
+var scripts = [
+  // local scripts
+
+  // 'scripts/**/*.js'
+
+	// bootstrap
+
+	// bootstrapPath + 'javascripts/bootstrap/affix.js',
+	// bootstrapPath + 'javascripts/bootstrap/alert.js',
+	// bootstrapPath + 'javascripts/bootstrap/button.js',
+	// bootstrapPath + 'javascripts/bootstrap/collapse.js',
+	bootstrapPath + 'javascripts/bootstrap/dropdown.js',
+	// bootstrapPath + 'javascripts/bootstrap/modal.js',
+	// bootstrapPath + 'javascripts/bootstrap/popover.js',
+	// bootstrapPath + 'javascripts/bootstrap/scrollspy.js',
+	// bootstrapPath + 'javascripts/bootstrap/tab.js',
+	// bootstrapPath + 'javascripts/bootstrap/tooltip.js',
+	// bootstrapPath + 'javascripts/bootstrap/transition.js',
 ];
 
 options.sass = {
@@ -43,6 +70,21 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(options.styleguide.destination + 'css'))
     .pipe(browserSync.stream());
 });
+
+// converts scripts into final minified file
+gulp.task('scripts', function(){
+	return gulp
+    .src(scripts)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(options.styleguide.destination + 'js'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(options.styleguide.destination + 'js'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
 
 // copies images to styleguide
 gulp.task('copy-images', function(){
@@ -80,6 +122,7 @@ gulp.task('serve', function() {
 
   // watch files and build/reload where needed
   gulp.watch(options.styleguide.source + '**/*.scss', ['sass', 'styleguide']);
+  gulp.watch(scripts, ['js']);
   gulp.watch(options.styleguide.source + 'images/**/*', ['copy-images']);
   gulp.watch(options.styleguide.builder + '/kss-assets/kss.scss', ['styleguide-sass']);
   gulp.watch([options.styleguide.source + '**/*', options.styleguide.builder + 'kss-assets/kss.css'], ['styleguide']);
@@ -87,7 +130,7 @@ gulp.task('serve', function() {
 });
 
 // when running `gulp build` for a static build
-gulp.task('build', ['sass', 'styleguide-sass', 'styleguide', 'copy-images', 'copy-fonts']);
+gulp.task('build', ['sass', 'styleguide-sass', 'scripts', 'styleguide', 'copy-images', 'copy-fonts']);
 
 // when running `gulp` to build, watch and re-build
 gulp.task('default', ['build', 'serve']);
